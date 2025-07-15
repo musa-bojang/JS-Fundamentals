a3aad654a1e3204dfc41ff62c473fe209fb8ddf8
musa-bojang <musabojang413@gmail.com>
2025-07-15 14:53:18 +0100

    Loop to languages

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -3,3 +3,5 @@
 const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (const line of lines) console.log(line);
+
+// This code iterates through an array of strings and prints each string to the console.
